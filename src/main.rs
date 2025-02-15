e053fbd2618d94ee28035aa05cc5396d981e10d1
liushuhang <liushuhang@17paipai.cn>
2025-02-15 11:51:39 +0800

    update name

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use mini_grep;
+use cerrper_grep;
 use std::{env, process};
 
 fn main() {
-    let config = mini_grep::Config::build(env::args()).unwrap_or_else(|err| {
+    let config = cerrper_grep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = mini_grep::run(config) {
+    if let Err(e) = cerrper_grep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
