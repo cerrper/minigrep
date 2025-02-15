07c3c2f8d9cbf5a26121fbf67080310ae4489f11
liushuhang <liushuhang@17paipai.cn>
2025-02-15 11:30:46 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::{env, process};
+use minigrep;
+
+fn main() {
+    let config = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
